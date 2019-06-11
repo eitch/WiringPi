c7033461c090c505f69c71534dc9a22722e3221c
Vicinzu <vc.turco@gmail.com>
2019-06-11 17:52:22 +0000

    Modifications for arm64 and custom cpuinfo-file

---
+++ README.md
@@ -0,0 +1,41 @@
+# Note
+This is a arm64 fork of the unofficial mirror of WiringPi to support ports (Python/Ruby/etc).
+
+Please do not email Gordon if you have issues, he will not be able to help.
+
+Pull-requests are not currently accepted, since this is a mirror.
+
+For support, comments, questions, etc please join the WiringPi Discord channel: https://discord.gg/SM4WUVG
+
+## How to usse
+
+Just compile like the original wiringPi and install it.
+
+In addition create a fake cpuinfo as /etc/wiringPi/cpuinfo with entries for Hardware and Revision, eg:
+```
+  Hardware	: BCM2710
+  Revision 	: a020d3
+```
+
+## wiringPi README
+
+Please note that the official way to get wiringPi is via git from
+git.drogon.net and not GitHub.
+
+ie.
+
+  git clone git://git.drogon.net/wiringPi
+
+The version of wiringPi held on GitHub by "Gadgetoid" is used to build the
+wiringPython, Ruby, Perl, etc. wrappers for these other languages. This
+version may lag the official Drogon release.  Pull requests may not be
+accepted to Github....
+
+Please see
+
+  http://wiringpi.com/
+
+for the official documentation, etc. and the best way to submit bug reports, etc.
+is by sending an email to projects@drogon.net
+
+Thanks!
--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -727,7 +727,7 @@ static void usingGpioMemCheck (const char *what)
 
 static void piGpioLayoutOops (const char *why)
 {
-  fprintf (stderr, "Oops: Unable to determine board revision from /proc/cpuinfo\n") ;
+  fprintf (stderr, "Oops: Unable to determine board revision from /etc/wiringpi/cpuinfo\n") ;
   fprintf (stderr, " -> %s\n", why) ;
   fprintf (stderr, " ->  You'd best google the error to find out why.\n") ;
 //fprintf (stderr, " ->  http://www.raspberrypi.org/phpBB3/viewtopic.php?p=184410#p184410\n") ;
@@ -744,8 +744,8 @@ int piGpioLayout (void)
   if (gpioLayout != -1)	// No point checking twice
     return gpioLayout ;
 
-  if ((cpuFd = fopen ("/proc/cpuinfo", "r")) == NULL)
-    piGpioLayoutOops ("Unable to open /proc/cpuinfo") ;
+  if ((cpuFd = fopen ("/etc/wiringpi/cpuinfo", "r")) == NULL)
+    piGpioLayoutOops ("Unable to open /etc/wiringpi/cpuinfo") ;
 
 // Start by looking for the Architecture to make sure we're really running
 //	on a Pi. I'm getting fed-up with people whinging at me because
@@ -956,8 +956,8 @@ void piBoardId (int *model, int *rev, int *mem, int *maker, int *warranty)
 
   (void)piGpioLayout () ;	// Call this first to make sure all's OK. Don't care about the result.
 
-  if ((cpuFd = fopen ("/proc/cpuinfo", "r")) == NULL)
-    piGpioLayoutOops ("Unable to open /proc/cpuinfo") ;
+  if ((cpuFd = fopen ("/etc/wiringpi/cpuinfo", "r")) == NULL)
+    piGpioLayoutOops ("Unable to open /etc/wiringpi/cpuinfo") ;
 
   while (fgets (line, 120, cpuFd) != NULL)
     if (strncmp (line, "Revision", 8) == 0)
