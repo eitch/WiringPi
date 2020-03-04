053fcb37c46ac549dd5caaae8d43b73e99c9cbc2
Robert von Burg <eitch@eitchnet.ch>
2020-03-04 14:45:44 +0100

    [Fix] Use /proc/cpuinfo again

--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -736,7 +736,7 @@ static void usingGpioMemCheck (const char *what)
 
 static void piGpioLayoutOops (const char *why)
 {
-  fprintf (stderr, "Oops: Unable to determine board revision from /etc/wiringpi/cpuinfo\n") ;
+  fprintf (stderr, "Oops: Unable to determine board revision from /proc/cpuinfo\n") ;
   fprintf (stderr, " -> %s\n", why) ;
   fprintf (stderr, " ->  You'd best google the error to find out why.\n") ;
 //fprintf (stderr, " ->  http://www.raspberrypi.org/phpBB3/viewtopic.php?p=184410#p184410\n") ;
@@ -753,8 +753,8 @@ int piGpioLayout (void)
   if (gpioLayout != -1)	// No point checking twice
     return gpioLayout ;
 
-  if ((cpuFd = fopen ("/etc/wiringpi/cpuinfo", "r")) == NULL)
-    piGpioLayoutOops ("Unable to open /etc/wiringpi/cpuinfo") ;
+  if ((cpuFd = fopen ("/proc/cpuinfo", "r")) == NULL)
+    piGpioLayoutOops ("Unable to open /proc/cpuinfo") ;
 
 // Start by looking for the Architecture to make sure we're really running
 //	on a Pi. I'm getting fed-up with people whinging at me because
@@ -965,8 +965,8 @@ void piBoardId (int *model, int *rev, int *mem, int *maker, int *warranty)
 
   (void)piGpioLayout () ;	// Call this first to make sure all's OK. Don't care about the result.
 
-  if ((cpuFd = fopen ("/etc/wiringpi/cpuinfo", "r")) == NULL)
-    piGpioLayoutOops ("Unable to open /etc/wiringpi/cpuinfo") ;
+  if ((cpuFd = fopen ("/proc/cpuinfo", "r")) == NULL)
+    piGpioLayoutOops ("Unable to open /proc/cpuinfo") ;
 
   while (fgets (line, 120, cpuFd) != NULL)
     if (strncmp (line, "Revision", 8) == 0)
